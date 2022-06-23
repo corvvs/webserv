2b553a449ee0265ca5c6832310a7ac3a4805259a
河田鷹介 <benefium@gmail.com>
2022-06-23 18:53:16 +0900

    文字集合クラス HTTP::CharFilter を追加

---
+++ src/utils/CharFilter.cpp
@@ -0,0 +1,182 @@
+#include "CharFilter.hpp"
+
+HTTP::CharFilter::CharFilter(const byte_string& chars) {
+    fill(chars);
+}
+
+HTTP::CharFilter::CharFilter(const char *chars) {
+    fill(byte_string(chars));
+}
+
+HTTP::CharFilter::CharFilter(uint8_t from, uint8_t to) {
+    fill(from, to);
+}
+
+HTTP::CharFilter::CharFilter(const CharFilter& other) {
+    *this = other;
+}
+
+HTTP::CharFilter& HTTP::CharFilter::operator=(const CharFilter& rhs) {
+    if (this != &rhs) {
+        memcpy(filter, rhs.filter, sizeof(uint64_t) * 4);
+    }
+    return *this;
+}
+
+HTTP::CharFilter& HTTP::CharFilter::operator=(const byte_string& rhs) {
+    fill(rhs);
+    return *this;
+}
+
+HTTP::CharFilter HTTP::CharFilter::operator|(const CharFilter& rhs) const {
+    CharFilter n(*this);
+    for (int i = 0; i < 4; ++i) {
+        n.filter[i] |= rhs.filter[i];
+    }
+    return n;
+}
+
+HTTP::CharFilter HTTP::CharFilter::operator&(const CharFilter& rhs) const {
+    CharFilter n(*this);
+    for (int i = 0; i < 4; ++i) {
+        n.filter[i] &= rhs.filter[i];
+    }
+    return n;
+}
+
+HTTP::CharFilter HTTP::CharFilter::operator^(const CharFilter& rhs) const {
+    CharFilter n(*this);
+    for (int i = 0; i < 4; ++i) {
+        n.filter[i] ^= rhs.filter[i];
+    }
+    return n;
+}
+
+HTTP::CharFilter HTTP::CharFilter::operator~() const {
+    CharFilter n(*this);
+    for (int i = 0; i < 4; ++i) {
+        n.filter[i] = ~n.filter[i];
+    }
+    return n;
+}
+
+HTTP::CharFilter HTTP::CharFilter::operator-(const CharFilter& rhs) const {
+    return *this & ~rhs;
+}
+
+void HTTP::CharFilter::fill(const byte_string& chars) {
+    memset(filter, 0, sizeof(uint64_t) * 4);
+    // 6 の出どころは 2^6 = 64.
+    for (byte_string::size_type i = 0; i < chars.size(); ++i) {
+        uint8_t c = chars[i];
+        filter[(c >> 6)] |= (uint64_t)1 << (c & (((uint64_t)1 << 6) - 1));
+    }
+}
+
+void HTTP::CharFilter::fill(uint8_t from, uint8_t to) {
+    memset(filter, 0, sizeof(uint64_t) * 4);
+    for (; from <= to; ++from) {
+        uint8_t c = from;
+        filter[(c >> 6)] |= (uint64_t)1 << (c & (((uint64_t)1 << 6) - 1));
+        if (from == to) { break; }
+    }
+}
+
+bool HTTP::CharFilter::includes(uint8_t c) const {
+    uint64_t x = (filter[(c >> 6)] & ((uint64_t)1 << (c & (((uint64_t)1 << 6) - 1))));
+    return x;
+}
+
+// 0x5555..
+// 0x5  = 0b0101
+// 0x55 = 0b01010101
+// 0xa  = 0b1010
+// 0xaa = 0b10101010
+
+// uint8_t x;
+// x & 0x55 = x & 0b01010101 -> x の偶数ビットのみ残す
+// x & 0xaa = x & 0b10101010 -> x の奇数ビットのみ残す
+// (x & 0xaa) >> 1 -> x の奇数ビットを偶数ビットにずらす
+// (x & 0x55) + ((x & 0xaa) >> 1) -> xの"隣り合うビット" [0,1] [2,3] [4,5] [6,7] の和
+
+// x = 167         = 0b 10 10 01 11
+// (x & 0x55)      = 0b 00 00 01 01
+// (x & 0xaa)      = 0b 10 10 00 10
+// (x & 0xaa) >> 1 = 0b 01 01 00 01
+// (x & 0x55) + ((x & 0xaa) >> 1)
+//                 = 0b 01 01 01 10 = y (popcount in 2bit)
+
+// 0x33            = 0b 00 11 00 11
+// 0xcc            = 0b 11 00 11 00
+// (y & 0x33)      = 0b 00 01 00 10
+// (y & 0xcc)      = 0b 01 00 01 00
+// (y & 0xcc) >> 2 = 0b 00 01 00 01
+// (y & 0x33) + ((y & 0xcc) >> 2)
+//                 = 0b 00 10 00 11 = z (popcount in 4bit)
+
+// 0x0f            = 0b 00 00 11 11
+// 0xf0            = 0b 11 11 00 00
+
+// 0x5555 = 0b0101010101010101 0xaaaa = 0b1010101010101010 1
+// 0x3333 = 0b0011001100110011 0xcccc = 0b1100110011001100 2
+// 0x0f0f = 0b0000111100001111 0xf0f0 = 0b1111000011110000 4
+// 0x00ff = 0b0000000011111111 0xff00 = 0b1111111100000000 8
+
+HTTP::byte_string::size_type    HTTP::CharFilter::size() const {
+    byte_string::size_type n = 0;
+    for (int i = 0; i < 4; ++i) {
+        uint64_t x = filter[i];
+        x = (x & 0x5555555555555555UL) + ((x & 0xaaaaaaaaaaaaaaaaUL) >> 1);
+        x = (x & 0x3333333333333333UL) + ((x & 0xccccccccccccccccUL) >> 2);
+        x = (x & 0x0f0f0f0f0f0f0f0fUL) + ((x & 0xf0f0f0f0f0f0f0f0UL) >> 4);
+        x = (x & 0x00ff00ff00ff00ffUL) + ((x & 0xff00ff00ff00ff00UL) >> 8);
+        x = (x & 0x0000ffff0000ffffUL) + ((x & 0xffff0000ffff0000UL) >> 16);
+        x = (x & 0x00000000ffffffffUL) + ((x & 0xffffffff00000000UL) >> 32);
+        n += x;
+    }
+    return n;
+}
+
+HTTP::byte_string   HTTP::CharFilter::str() const {
+    HTTP::byte_string   out;
+    out.reserve(size());
+    for (unsigned int i = 0; i < 4 * 8 * sizeof(uint64_t); ++i) {
+        uint8_t c = i;
+        if (includes(c)) { out.push_back(c); }
+    }
+    return out;
+}
+
+std::ostream&   operator<<(std::ostream& ost, const HTTP::CharFilter& f) {
+    return ost << "(" << f.size() << "):[" << f.str() << "]";
+}
+
+const HTTP::CharFilter HTTP::CharFilter::alpha_low  = HTTP::Charset::alpha_low;
+const HTTP::CharFilter HTTP::CharFilter::alpha_up   = HTTP::Charset::alpha_up;
+const HTTP::CharFilter HTTP::CharFilter::alpha      = HTTP::Charset::alpha;
+const HTTP::CharFilter HTTP::CharFilter::digit      = HTTP::Charset::digit;
+const HTTP::CharFilter HTTP::CharFilter::hexdig     = HTTP::Charset::hexdig;
+const HTTP::CharFilter HTTP::CharFilter::unreserved = HTTP::Charset::unreserved;
+const HTTP::CharFilter HTTP::CharFilter::gen_delims = HTTP::Charset::gen_delims;
+const HTTP::CharFilter HTTP::CharFilter::sub_delims = HTTP::Charset::sub_delims;
+const HTTP::CharFilter HTTP::CharFilter::tchar      = HTTP::Charset::tchar;
+const HTTP::CharFilter HTTP::CharFilter::sp         = HTTP::Charset::sp;
+const HTTP::CharFilter HTTP::CharFilter::ws         = HTTP::Charset::ws;
+const HTTP::CharFilter HTTP::CharFilter::crlf       = HTTP::Charset::crlf;
+const HTTP::CharFilter HTTP::CharFilter::cr         = "\r";
+const HTTP::CharFilter HTTP::CharFilter::lf         = HTTP::Charset::lf;
+const HTTP::CharFilter HTTP::CharFilter::htab       = "\t";
+const HTTP::CharFilter HTTP::CharFilter::dquote     = "\"";
+const HTTP::CharFilter HTTP::CharFilter::bslash     = "\\";
+const HTTP::CharFilter HTTP::CharFilter::obs_text   = HTTP::CharFilter(0x80, 0xff);
+const HTTP::CharFilter HTTP::CharFilter::vchar      = HTTP::CharFilter(0x21, 0x7e);
+const HTTP::CharFilter HTTP::CharFilter::qdtext     = HTTP::CharFilter::vchar | HTTP::CharFilter::htab | HTTP::CharFilter::obs_text - "\"\\";
+const HTTP::CharFilter HTTP::CharFilter::quoted_right = HTTP::CharFilter::vchar | " \t" | HTTP::CharFilter::obs_text;
+
+// parameter      = token "=" ( token / quoted-string )
+// quoted-string  = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+// qdtext         = HTAB / SP / %x21 / %x23-5B / %x5D-7E / obs-text
+//                ;               !     #-[        ]-~
+//                ; HTAB + 表示可能文字, ただし "(ダブルクオート) と \(バッスラ) を除く
+// obs-text       = %x80-FF ; extended ASCII
+// quoted-pair    = "\" ( HTAB / SP / VCHAR / obs-text )
---
+++ src/utils/CharFilter.hpp
@@ -0,0 +1,79 @@
+#ifndef CHARFILTER_HPP
+# define CHARFILTER_HPP
+# include <iostream>
+# include <string>
+# include <map>
+# include "http.hpp"
+
+// 全体で共通して使うenum, 型, 定数, フリー関数など
+
+namespace HTTP {
+
+    // 単純な文字集合クラス
+    class CharFilter {
+    private:
+        uint64_t    filter[4];
+
+    public:
+        CharFilter(const byte_string& chars);
+        CharFilter(const char* chars);
+        CharFilter(const CharFilter& other);
+        // by exclusive char-range
+        CharFilter(uint8_t from, uint8_t to);
+
+        CharFilter& operator=(const CharFilter& rhs);
+        CharFilter& operator=(const byte_string& rhs);
+
+        // set union
+        CharFilter  operator|(const CharFilter& rhs) const;
+        // set intersection
+        CharFilter  operator&(const CharFilter& rhs) const;
+        // set symmetric difference
+        CharFilter  operator^(const CharFilter& rhs) const;
+        // set complement
+        CharFilter  operator~() const;
+        CharFilter  operator-(const CharFilter& rhs) const;
+
+        // 文字集合を文字列`chars`を使って初期化する
+        void        fill(const byte_string& chars);
+        void        fill(uint8_t from, uint8_t to);
+        // `c` が文字集合に含まれるかどうか
+        bool        includes(uint8_t c) const;
+        // 文字集合のサイズ
+        byte_string::size_type  size() const;
+
+        // アルファベット・小文字
+        static const CharFilter alpha_low;
+        // アルファベット・大文字
+        static const CharFilter alpha_up;
+        // アルファベット
+        static const CharFilter alpha;
+        // 数字
+        static const CharFilter digit;
+        // 16進数における数字
+        static const CharFilter hexdig;
+        // HTTPにおける非予約文字
+        static const CharFilter unreserved;
+        static const CharFilter gen_delims;
+        static const CharFilter sub_delims;
+        static const CharFilter tchar;
+        static const CharFilter sp;
+        static const CharFilter ws;
+        static const CharFilter crlf;
+        static const CharFilter cr;
+        static const CharFilter lf;
+        static const CharFilter htab;
+        static const CharFilter dquote;
+        static const CharFilter bslash;
+        static const CharFilter obs_text;
+        static const CharFilter vchar;
+        static const CharFilter qdtext;
+        static const CharFilter quoted_right;
+
+        byte_string str() const;
+    };
+}
+
+std::ostream&   operator<<(std::ostream& ost, const HTTP::CharFilter& f);
+
+#endif
