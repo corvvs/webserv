ecee5fad368300754cfb54bec90d37a533e61f81
Ryomanba <datemanba3@gmail.com>
2022-06-26 19:49:13 +0900

    #7 テスト用のスクリプト追加

---
+++ config_parser/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+CYAN="\033[36m"
+RESET="\033[0m"
+
+CONFIG='../conf'
+
+
+find $CONFIG/invalid -name "*.conf" | sort > tempfile
+echo "=====INVALID CONFIG TEST====="
+while read line
+do
+    echo -en $CYAN"[CASE] "
+    filename=(${line//\// })
+    echo ${filename[3]}
+    echo -en $RESET
+
+    ./a.out $line
+done < tempfile
+
+find $CONFIG/valid -name "*.conf" | sort > tempfile
+echo "=====VALID CONFIG TEST====="
+while read line
+do
+    echo -en $CYAN"[CASE] "
+    filename=(${line//\// })
+    echo ${filename[3]}
+    echo -en $RESET
+
+    ./a.out $line
+done < tempfile
+rm tempfile
