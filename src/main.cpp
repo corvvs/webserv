8d12cd88c2ed0c8420c3ca2d91f89006f29823be
河田鷹介 <benefium@gmail.com>
2022-07-05 13:31:04 +0900

    [expserv -> webserv] serverディレクトリを新設

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
-#include "socket/SocketConnected.hpp"
-#include "socket/SocketListening.hpp"
-#include "string"
-#include <iostream>
+#include "event/Eventkqueueloop.hpp"
+#include "event/Eventpollloop.hpp"
 #include "event/Eventselectloop.hpp"
+#include "server/HTTPServer.hpp"
+#define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2 * !!(condition)]))
 
-int main(void) {
-  IObserver *i_observer = new EventSelectLoop;
-  (void )i_observer;
+void assert_sizeoftype() {
+    BUILD_BUG_ON(sizeof(u8t) != 1);
+    BUILD_BUG_ON(sizeof(u16t) != 2);
+    BUILD_BUG_ON(sizeof(u32t) != 4);
+    BUILD_BUG_ON(sizeof(u64t) != 8);
+}
+
+int main() {
+    assert_sizeoftype();
+
+    // HTTPServer  http_server(new EventKqueueLoop());
+    HTTPServer http_server(new EventPollLoop());
+    // HTTPServer  http_server(new EventSelectLoop());
+
+    http_server.listen(SD_IP4, ST_TCP, 8080);
+    http_server.listen(SD_IP4, ST_TCP, 8081);
+    http_server.listen(SD_IP4, ST_TCP, 8082);
+    http_server.listen(SD_IP4, ST_TCP, 8083);
+    http_server.listen(SD_IP4, ST_TCP, 8084);
+
+    http_server.run();
 }
