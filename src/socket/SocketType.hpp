70f2a7e0ec45a7dbf19aad79efcda018437e4ccc
河田鷹介 <benefium@gmail.com>
2022-07-05 11:49:57 +0900

    uint_**_t をu**t に変更

--- src/socket/SocketType.hpp
+++ src/socket/SocketType.hpp
@@ -18,18 +18,8 @@ enum t_socket_domain { SD_IP4, SD_IP6, SD_UNIX };
 enum t_socket_type { ST_TCP, ST_UDP, ST_STREAM };
 
 typedef int t_fd;
-typedef uint16_t t_port;
-typedef uint32_t t_addressv4;
-
-enum t_socket_operation { SHMT_NONE, SHMT_READ, SHMT_WRITE, SHMT_EXCEPTION };
-
-class ISocketLike;
-
-struct t_socket_reservation {
-    ISocketLike *sock;
-    t_socket_operation from;
-    t_socket_operation to;
-};
+typedef u16t t_port;
+typedef u32t t_addressv4;
 
 int sockdomain(t_socket_domain d);
 int socktype(t_socket_type t);
