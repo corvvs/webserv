09047748234475ded5cc875ac52b69256d75cfb2
河田鷹介 <benefium@gmail.com>
2022-07-17 11:24:27 +0900

    余計な変更を差し戻す

--- src/socket/SocketUNIX.cpp
+++ src/socket/SocketUNIX.cpp
@@ -1,9 +1,6 @@
 #include "SocketUNIX.hpp"
-#include "../utils/test_common.hpp"
 
-SocketUNIX::SocketUNIX(int fd) : ASocket(fd, SD_UNIX, ST_STREAM) {
-    DXOUT("SockUNIX: " << fd);
-}
+SocketUNIX::SocketUNIX(int fd) : ASocket(fd, SD_UNIX, ST_STREAM) {}
 
 std::pair<SocketUNIX *, t_fd> SocketUNIX::socket_pair() {
     int fds[2];
@@ -12,24 +9,4 @@ std::pair<SocketUNIX *, t_fd> SocketUNIX::socket_pair() {
         throw std::runtime_error("failed to make sockerpair");
     }
     return std::pair<SocketUNIX *, t_fd>(new SocketUNIX(fds[0]), fds[1]);
-}
-
-ssize_t SocketUNIX::send(const void *buffer, size_t len, int flags) {
-    return ::send(fd, buffer, len, flags);
-}
-
-ssize_t SocketUNIX::receive(void *buffer, size_t len, int flags) {
-    return ::recv(fd, buffer, len, flags);
-}
-
-int SocketUNIX::shutdown() {
-    return ::shutdown(get_fd(), SHUT_RDWR);
-}
-
-int SocketUNIX::shutdown_write() {
-    return ::shutdown(get_fd(), SHUT_WR);
-}
-
-int SocketUNIX::shutdown_read() {
-    return ::shutdown(get_fd(), SHUT_RD);
 }
--- src/socket/SocketUNIX.hpp
+++ src/socket/SocketUNIX.hpp
@@ -7,6 +7,8 @@
 // [責務]
 class SocketUNIX : public ASocket {
 private:
+    SocketUNIX();
+    SocketUNIX(t_socket_domain sdomain, t_socket_type stype);
     SocketUNIX(const SocketUNIX &other);
     SocketUNIX &operator=(const SocketUNIX &rhs);
 
@@ -14,13 +16,6 @@ public:
     SocketUNIX(int fd);
 
     static std::pair<SocketUNIX *, t_fd> socket_pair();
-
-    ssize_t send(const void *buffer, size_t len, int flags);
-    ssize_t receive(void *buffer, size_t len, int flags);
-
-    int shutdown();
-    int shutdown_write();
-    int shutdown_read();
 };
 
 #endif
