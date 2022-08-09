93861afc7c901d6493eeb173cd7279c9ebfdf8d1
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-09 17:53:57 +0900

    format

--- test_case/utils/gtest_file.cpp
+++ test_case/utils/gtest_file.cpp
@@ -1,6 +1,5 @@
-#include "gtest/gtest.h"
 #include "../../src/utils/File.hpp"
-
+#include "gtest/gtest.h"
 
 TEST(get_directory_name, example) {
     EXPECT_EQ(file::get_directory_name("path/file"), "path");
