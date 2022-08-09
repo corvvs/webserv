3bab10ba70d1368b73cd37e441933a639cd6ff47
河田鷹介 <benefium@gmail.com>
2022-08-09 14:41:13 +0900

    formatter

--- test_case/header/test_header_http.cpp
+++ test_case/header/test_header_http.cpp
@@ -3,14 +3,7 @@
 
 // ヘッダ名の前, ':'の後, 値の前後のスペースは不問
 TEST(header_http, spaces_ok) {
-    const char* strs[] = {
-        "Host: aaa",
-        "Host: ",
-        "Host:",
-        " Host: aaa",
-        "Host: 123123",
-        NULL
-    };
+    const char *strs[] = {"Host: aaa", "Host: ", "Host:", " Host: aaa", "Host: 123123", NULL};
     for (size_t i = 0; strs[i]; ++i) {
         HTTP::byte_string s = HTTP::strfy(strs[i]);
         const HTTP::light_string l(s);
@@ -22,13 +15,7 @@ TEST(header_http, spaces_ok) {
 
 // ':'の前のスペース, 空のヘッダ名はNG
 TEST(header_http, spaces_ko) {
-    const char* strs[] = {
-        "",
-        "    ",
-        ":",
-        "Host : aaa",
-        NULL
-    };
+    const char *strs[] = {"", "    ", ":", "Host : aaa", NULL};
     for (size_t i = 0; strs[i]; ++i) {
         HTTP::byte_string s = HTTP::strfy(strs[i]);
         const HTTP::light_string l(s);
@@ -39,18 +26,16 @@ TEST(header_http, spaces_ko) {
 }
 
 TEST(header_http, token_ok) {
-    const char* strs[] = {
-        "Host:",
-        "host:",
-        "hos1:",
-        "123:",
-        "$$$:",
-        "!#$%&'*+-.^_`|~:",
-        // 本当はKOだけどブラウザが許可してるっぽいので...
-        "Ho st:",
-        "Ho@st:",
-        NULL
-    };
+    const char *strs[] = {"Host:",
+                          "host:",
+                          "hos1:",
+                          "123:",
+                          "$$$:",
+                          "!#$%&'*+-.^_`|~:",
+                          // 本当はKOだけどブラウザが許可してるっぽいので...
+                          "Ho st:",
+                          "Ho@st:",
+                          NULL};
     for (size_t i = 0; strs[i]; ++i) {
         HTTP::byte_string s = HTTP::strfy(strs[i]);
         const HTTP::light_string l(s);
@@ -85,13 +70,7 @@ TEST(header_http, size_ko) {
 
 // ヘッダ値の前後の空白は値ではない
 TEST(header_http, value_ok) {
-    const char* strs[] = {
-        "Host: aiueo",
-        "Host:aiueo",
-        "Host:aiueo    ",
-        "host: aiueo",
-        NULL
-    };
+    const char *strs[] = {"Host: aiueo", "Host:aiueo", "Host:aiueo    ", "host: aiueo", NULL};
     for (size_t i = 0; strs[i]; ++i) {
         const HTTP::byte_string s = HTTP::strfy(strs[i]);
         const HTTP::light_string l(s);
@@ -107,12 +86,7 @@ TEST(header_http, value_ok) {
 
 // ヘッダ値内部の空白は値
 TEST(header_http, value_ok_2) {
-    const char* strs[] = {
-        "Host:a i u e o",
-        "Host: a i u e o",
-        "Host:a i u e o    ",
-        NULL
-    };
+    const char *strs[] = {"Host:a i u e o", "Host: a i u e o", "Host:a i u e o    ", NULL};
     for (size_t i = 0; strs[i]; ++i) {
         const HTTP::byte_string s = HTTP::strfy(strs[i]);
         const HTTP::light_string l(s);
@@ -128,13 +102,7 @@ TEST(header_http, value_ok_2) {
 
 // ヘッダの重複
 TEST(header_http, duplication) {
-    const char* strs[] = {
-        "Host:aiueo",
-        "Host: 1234",
-        "Host: xyz",
-        " hoST:beepbeep",
-        NULL
-    };
+    const char *strs[] = {"Host:aiueo", "Host: 1234", "Host: xyz", " hoST:beepbeep", NULL};
     HeaderHolderHTTP holder;
     for (size_t i = 0; strs[i]; ++i) {
         const HTTP::byte_string s = HTTP::strfy(strs[i]);
