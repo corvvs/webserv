420117137577a7f8f84ae103245f2b761cde0aa2
河田鷹介 <benefium@gmail.com>
2022-08-15 17:46:52 +0900

    RequestMatchingResultのデフォルトコンストラクタを消したことへの対応

--- test_case/config/original_directive.cpp
+++ test_case/config/original_directive.cpp
@@ -134,9 +134,8 @@ http { \
 
     {
         TestParam tp(HTTP::METHOD_GET, "/ruby/blank.rb", HTTP::V_1_1, "localhost", "80");
-        RequestMatchingResult res;
         EXPECT_NO_THROW({
-            res = rm.request_match(configs[hp], tp);
+            RequestMatchingResult res = rm.request_match(configs[hp], tp);
             EXPECT_EQ(HTTP::strfy("./cgi/ruby"), res.cgi_resource.root);
             EXPECT_EQ(HTTP::strfy("/blank.rb"), res.cgi_resource.script_name);
             EXPECT_EQ(HTTP::strfy(""), res.cgi_resource.path_info);
@@ -145,9 +144,8 @@ http { \
 
     {
         TestParam tp(HTTP::METHOD_GET, "/ruby/blank.rb/path/after", HTTP::V_1_1, "localhost", "80");
-        RequestMatchingResult res;
         EXPECT_NO_THROW({
-            res = rm.request_match(configs[hp], tp);
+            RequestMatchingResult res = rm.request_match(configs[hp], tp);
             EXPECT_EQ(HTTP::strfy("./cgi/ruby"), res.cgi_resource.root);
             EXPECT_EQ(HTTP::strfy("/blank.rb"), res.cgi_resource.script_name);
             EXPECT_EQ(HTTP::strfy("/path/after"), res.cgi_resource.path_info);
@@ -179,7 +177,6 @@ http { \
 
     {
         TestParam tp(HTTP::METHOD_GET, "/ruby/not_exist.rb/path/after", HTTP::V_1_1, "localhost", "80");
-        RequestMatchingResult res;
         EXPECT_THROW(rm.request_match(configs[hp], tp), http_error);
     }
 }
