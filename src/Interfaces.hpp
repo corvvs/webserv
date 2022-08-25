9ea840af9bfe93515b422ee7cdf1324b9f25c801
河田鷹介 <benefium@gmail.com>
2022-08-25 09:44:18 +0900

    add忘れ

--- src/Interfaces.hpp
+++ src/Interfaces.hpp
@@ -1,6 +1,7 @@
 #ifndef INTERFACES_HPP
 #define INTERFACES_HPP
 
+#include "interface/IDataSender.hpp"
 #include "interface/IObserver.hpp"
 #include "interface/IOriginator.hpp"
 #include "interface/IRequestMatcher.hpp"
