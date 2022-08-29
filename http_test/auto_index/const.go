6c18270ddbe333b71a77e1a32e31f3d1c7516771
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-08-29 21:30:56 +0900

    add: auto index test

---
+++ http_test/auto_index/const.go
@@ -0,0 +1,21 @@
+package auto_index
+
+import _ "embed"
+
+const (
+	webservPort = "8080"
+	validHeader = "Host: localhost:8080\r\n" +
+		"User-Agent: curl/7.77.0\r\n" +
+		"Accept: */*\r\n" +
+		"Connection: close\r\n\r\n"
+	requestLineGetRootHTTP11 = "GET / HTTP/1.1\r\n"
+)
+
+var (
+	//go:embed html/index.html
+	indexHtml []byte
+	//go:embed html/sample.html
+	sampleHtml []byte
+	//go:embed html/dir/dir.html
+	dirHtml []byte
+)
