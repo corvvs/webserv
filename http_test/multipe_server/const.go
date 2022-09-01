1787c6a424398eac7514b13c7a4914260cf6fa44
nao215912 <79066275+nao215912@users.noreply.github.com>
2022-09-02 00:15:14 +0900

    add_multiple_server_test

---
+++ http_test/multipe_server/const.go
@@ -0,0 +1,21 @@
+package multipe_server
+
+import _ "embed"
+
+const (
+	webservPort = "8080"
+	validHeader = "User-Agent: curl/7.77.0\r\n" +
+		"Accept: */*\r\n" +
+		"Connection: close\r\n\r\n"
+)
+
+var (
+	//go:embed a/index.html
+	aIndexHtml []byte
+	//go:embed a/sample.html
+	aSampleHtml []byte
+	//go:embed b/index.html
+	bIndexHtml []byte
+	//go:embed b/sample.html
+	bSampleHtml []byte
+)
---
+++ http_test/multipe_server/mutiple_server_test.go
@@ -0,0 +1,64 @@
+package multipe_server
+
+import (
+	"http_test/client"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestSimple(t *testing.T) {
+	tests := []struct {
+		name       string
+		request    string
+		statusCode int
+		body       []byte
+	}{
+		{
+
+			name:       "a_index",
+			request:    "GET / HTTP/1.1\r\n" + "Host: a\r\n" + validHeader,
+			statusCode: http.StatusOK,
+			body:       aIndexHtml,
+		},
+		{
+
+			name:       "a_sample",
+			request:    "GET /sample.html HTTP/1.1\r\n" + "Host: a\r\n" + validHeader,
+			statusCode: http.StatusOK,
+			body:       aSampleHtml,
+		},
+		{
+
+			name:       "b_index",
+			request:    "GET / HTTP/1.1\r\n" + "Host: b\r\n" + validHeader,
+			statusCode: http.StatusOK,
+			body:       bIndexHtml,
+		},
+		{
+
+			name:       "b_sample",
+			request:    "GET /sample.html HTTP/1.1\r\n" + "Host: b\r\n" + validHeader,
+			statusCode: http.StatusOK,
+			body:       bSampleHtml,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, err := client.NewClient("default", tt.request, webservPort)
+			if err != nil {
+				t.Fatal(err)
+			}
+			res, err := c.Run()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if res.StatusCode != tt.statusCode {
+				t.Errorf("unexpected status code got = %d, want %d", res.StatusCode, tt.statusCode)
+			}
+			if !reflect.DeepEqual(res.Body, tt.body) {
+				t.Errorf("unexpected body got = %s, want %s", string(res.Body), string(tt.body))
+			}
+		})
+	}
+}
