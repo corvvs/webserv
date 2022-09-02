df390b1612da923113f1938d2b5261fb37aa77f1
河田鷹介 <benefium@gmail.com>
2022-09-02 12:52:55 +0900

    コードを単純化

--- src/communication/ControlHeaderHTTP.cpp
+++ src/communication/ControlHeaderHTTP.cpp
@@ -799,7 +799,7 @@ HTTP::light_string HTTP::CH::CookieEntry::parse_name_value(const light_string &s
     QVOUT(work);
     // cookie-value の捕捉
     // cookie-value  = *cookie-octet / ( DQUOTE *cookie-octet DQUOTE )
-    const bool maybe_quoted = work.size() > 0 && work[0] == '"';
+    const bool maybe_quoted = work.starts_with("\"");
     if (maybe_quoted) {
         // ( DQUOTE *cookie-octet DQUOTE )
         // かもしれない
@@ -809,7 +809,7 @@ HTTP::light_string HTTP::CH::CookieEntry::parse_name_value(const light_string &s
     work                            = work.substr(cookie_value.size());
     QVOUT(work);
     if (maybe_quoted) {
-        if (work.size() == 0 || work[0] != '"') {
+        if (!work.starts_with("\"")) {
             error = minor_error::make("away; no closing dquote", HTTP::STATUS_BAD_REQUEST);
             return work;
         }
